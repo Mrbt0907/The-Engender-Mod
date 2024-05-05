df954dfe6f45da988aad6ef677d9ffcd5841fa65
gnang <gnang@Ender-puter>
2024-05-05 13:17:25 -0400

    e

--- src/main/java/net/minecraft/AgeOfMinecraft/blocks/BlockFusionCrafter.java
+++ src/main/java/net/minecraft/AgeOfMinecraft/blocks/BlockFusionCrafter.java
@@ -1,13 +1,9 @@
 package net.minecraft.AgeOfMinecraft.blocks;
 import java.util.List;
-import java.util.Random;
-
 import javax.annotation.Nullable;
 
 import net.minecraft.AgeOfMinecraft.EngenderMod;
 import net.minecraft.AgeOfMinecraft.entity.EntityManaOrb;
-import net.minecraft.AgeOfMinecraft.registry.BlockRegistry;
-import net.minecraft.AgeOfMinecraft.registry.CreativeTabRegistry;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -16,7 +12,6 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
